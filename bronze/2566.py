c2b7f3e50842fc83003e928977ae4e95eced7066
Hayoun Jang <hhayounn1114@gmail.com>
2024-02-17 18:19:53 +0900

    bronze

---
+++ bronze/2566.py
@@ -0,0 +1,22 @@
+# 백준 2566번 최댓값
+
+arr = []
+
+for i in range(9):
+    arr.append(list(map(int, input().split())))
+
+max = arr[0][0]
+x = 0
+y = 0
+
+for i in range(9):
+    for j in range(9):
+        if arr[i][j] > max:
+            max = arr[i][j]
+            x = i
+            y = j
+
+print(max)
+print("%d %d" % (x, y))
+
+
